914a3ee329560bcbf122a75f336ad4ab43ab690c
Hiroaki Nakamoto <nakamoto@white-son.com>
2020-02-27 18:07:31 +0900

    キーワード指定に対応

--- app.py
+++ app.py
@@ -11,15 +11,23 @@ slack_token = sys.argv[1]
 allowd_chanels_array = []
 if len(sys.argv) > 2:
   allowd_chanels = sys.argv[2]
-  allowd_chanels_array = allowd_chanels.split(',')
+  if len(allowd_chanels) > 0:
+    allowd_chanels_array = allowd_chanels.split(',')
 
-print(allowd_chanels_array)
+if len(sys.argv) > 3:
+  keywords = sys.argv[3]
+  if len(keywords) > 0:
+      keywords_array = keywords.split(',')
+
+#print(allowd_chanels_array)
+#print(keywords_array)
 
 sc = SlackClient(slack_token)
 
 if sc.rtm_connect():
   while sc.server.connected is True:
     response = sc.rtm_read()
+    #print(response)
     for data in response:
       if 'type' in data and 'text' in data:
         if data['type'] == 'message':
@@ -29,12 +37,12 @@ if sc.rtm_connect():
             "channels.info",
             channel=data['channel']
           )
-          print(res['channel']['name'])
+          #print(res['channel']['name'])
           channel_name = res['channel']['name']
 
           # チャネル指定があった場合は制限をかける
           if len(allowd_chanels_array) > 0 and channel_name not in allowd_chanels_array:
-              continue
+            continue
 
           user_name = ''
           if 'user' in data:
@@ -47,9 +55,21 @@ if sc.rtm_connect():
             user_name = res['user']['real_name']
 
           message = data['text'].strip()
+
+          # キーワード指定があった場合は制限をかける
+          if len(keywords_array) > 0:
+            found = False
+            for keyword in keywords_array:
+                if len(keyword) > 0 and keyword in message:
+                    found = True
+                    break
+            if found == False:
+              continue
+              
           #message = re.sub(r'[!-/]', "", message)
           #message = re.sub(r'[\[-\`]', "", message)
           #message = re.sub(r'[\{-\~]', "", message)
+          message = re.sub(r'<.+>', "", message)
           message = re.sub('\n', " ", message)
           message = re.sub(';', " ", message)
           message = re.sub('\'', " ", message)
